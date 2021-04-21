e80dde4553b1889b34e5b05c4b50168f5033a21c
MahdiRashidi <rashidi.mahdi2001@gmail.com>
2021-04-21 17:46:45 +0430

    init

---
+++ Assets/serverManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using gamingCloud.Network;
+using UnityEngine;
+
+public class serverManager : GCMultiPlayer
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToMultiPlayerServer();
+    }
+
+    public override void ConnectedToServer()
+    {
+        JoinToServer(new RoomSetting());
+    }
+    public override void OnJoined()
+    {
+        GameObject spnPoint = GameObject.Find("spn-" + spawnIndex);
+        GameObject.FindGameObjectWithTag("Player").transform.position = spnPoint.transform.position;
+    }
+    void Update()
+    {
+
+    }
+}
